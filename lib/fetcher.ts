0b7d6ccc9fbfbc67b4de2074443738cda7b2558e
deeninsome <syihab855@gmail.com>
2025-12-02 16:19:50 +0700

    update auth

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,12 +1,19 @@
 
-export const fetcher = async (url: string) => {
-  const res = await fetch(url);
+export const fetcher = async (url: string, options?: RequestInit) => {
+  const res = await fetch(url, {
+    ...options,
+  });
 
-  // Jika respons tidak OK, lempar Error dengan detail
   if (!res.ok) {
     const errorData = await res.json().catch(() => ({}));
-    const error = new Error(errorData.error || 'Terjadi kesalahan saat fetching data');
+
+    const error = new Error(errorData.message || errorData.error || `Error ${res.status}: Gagal fetching data`);
     (error as any).status = res.status;
+
+    if (res.status === 401) {
+      (error as any).isAuthError = true;
+    }
+
     throw error;
   }
 
